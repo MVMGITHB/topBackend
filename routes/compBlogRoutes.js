e890e01a8daffbb2d55c0336910694365b10c1e8
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-05-14 17:45:13 +0530

    sdjfkdsjf

--- routes/compBlogRoutes.js
+++ routes/compBlogRoutes.js
@@ -1,11 +1,12 @@
 // routes/compBlogRoutes.js
 const express = require("express");
 const router = express.Router();
-const {createCompBlog,getAllCompBlogs,getCompBlogBySlug,updateCompBlog,deleteCompBlog,filterBLog,updateStatus,filterbysubcateory} = require("../controllers/CompBlogController");
+const {createCompBlog,getAllCompBlogs,getCompBlogBySlug,updateCompBlog,deleteCompBlog,filterBLog,updateStatus,filterbysubcateory,filterBLog1} = require("../controllers/CompBlogController");
 
 router.post("/createCompblogs", createCompBlog);
 router.get("/getALlcompblogs", getAllCompBlogs);
 router.get("/filter/:slug", filterBLog);
+router.get("/filter1/:slug", filterBLog1);
 router.get("/filterBlog/:slug", filterbysubcateory);
 router.get("/getOnecompblogs/:slug", getCompBlogBySlug);
 router.put("/updatecompblogs/:slug", updateCompBlog);
